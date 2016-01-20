fc3ed44ada8e85051231401b4db0939128d71373
Mark Grimes <kknb1056@gmail.com>
2016-01-20 17:38:51 +0000

    Switch to iostream

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
-#include <cstdio>
-#include <array>
+#include <iostream>
+#include <iomanip>
+#include <bitset>
 
 #include "FloatBits.h"
 
 int main( int argc, char* argv[] )
 {
-	printf( "** Starting c++ test\n" );
+	std::cout << "** Starting c++ test" << "\n";
 	FloatBits blah;
 	blah.valueFrom(78);
-	printf( "%f from\n", blah.value() );
+	std::cout << blah.value() << "\n";
 	blah.set(31,true);
-	printf( "%f from\n", blah.value() );
+	std::cout << blah.value() << "\n";
 	blah.set(25,false);
-	printf( "%f from\n", blah.value() );
+	std::cout << blah.value() << "\n";
+	blah.valueFrom(-80);
+	std::bitset<sizeof(int)*8> intBitset(blah.asInt());
+	std::cout << "Bitset " << intBitset << "\n";
+	std::cout << "As binary " << blah.asBinary() << "\n";
+	std::cout << std::hex << blah.asInt() << " " << std::dec << blah.asInt() << "\n";
 	return 0;
 }
 
